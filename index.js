5c945a2d08eb7b1a7791acc941683141977f85cd
chris <chris.lu06@gmail.com>
2021-12-19 18:53:00 -0600

    trying to get express to serve vue files

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.use(bodyParser.json());
 app.use(express.static(process.cwd() + '/web-ui/dist'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+//   res.send('Hello World!')
+	res.sendFile('/web-ui/dist/index.html')
 })
 
 app.get('/api/users', (req, res) => {
